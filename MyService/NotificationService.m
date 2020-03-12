e391bc1ab2d0fb10bcfd983a9522f727759da720
Norvon <norvon@126.com>
2020-03-12 11:07:53 +0800

    添加说明

--- MyService/NotificationService.m
+++ MyService/NotificationService.m
@@ -26,9 +26,27 @@
     // Modify the notification content here...
     //    self.bestAttemptContent.title = [NSString stringWithFormat:@"%@ [modified]", self.bestAttemptContent.title];
     
+    //    {
+    //        "aps": {
+    //            "badge": 1,
+    //            "alert": {
+    //                "title": "测试抬头title",
+    //                "subtitle": "测试抬头subtitle",
+    //                "body": "主题body 0"
+    //            },
+    //            "sound": "defult",
+    //            "category": "realtime",
+    //            "mutable-content": "1" // 为1的时候走自定义消息解析
+    //        },
+    //        "media": {
+    //            "type": "image",
+    //            "url": "https://www.baidu.com/img/bd_logo1.png"
+    //        }
+    //    }
+    
+    // 记得修改当前target 的 ats
     
     NSDictionary *dic = [request.content.userInfo valueForKey:@"media"] ;
-    
     if ([dic isKindOfClass:[NSDictionary class]] == NO) {
         self.contentHandler(self.bestAttemptContent);
         return;
@@ -46,9 +64,10 @@
         NSURL *url = [NSURL URLWithString:urlStr];
         NSURLSessionConfiguration *config = [NSURLSessionConfiguration defaultSessionConfiguration];
         NSURLSession *session = [NSURLSession sessionWithConfiguration:config];
-        NSURLSessionDataTask *task = [session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        NSURLSessionDataTask *task = [session dataTaskWithURL:url
+                                            completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
             if (!error) {
-                //2. 保存数据, 不可以存储到不存在的路径
+                
                 NSString *path = [NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, YES).firstObject stringByAppendingPathComponent:@"logo.png"];
                 UIImage *image = [UIImage imageWithData:data];
                 
@@ -56,7 +75,9 @@
                 
                 [UIImageJPEGRepresentation(image, 1) writeToFile:path options:NSAtomicWrite error:&err];
                 //3. 添加附件
-                UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:@"remote-atta1" URL:[NSURL fileURLWithPath:path] options:nil error:&err];
+                UNNotificationAttachment *attachment = [UNNotificationAttachment attachmentWithIdentifier:@"remote-atta1"
+                                                                                                      URL:[NSURL fileURLWithPath:path]
+                                                                                                  options:nil error:&err];
                 if (attachment) {
                     self.bestAttemptContent.attachments = @[attachment];
                 }
@@ -64,8 +85,11 @@
             self.contentHandler(self.bestAttemptContent);
         }];
         [task resume];
-
     }
+    else {
+        self.contentHandler(self.bestAttemptContent);
+    }
+    
 }
 
 - (void)serviceExtensionTimeWillExpire {
